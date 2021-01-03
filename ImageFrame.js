4c992cab74e7a102368cbcd6b853a8eb64ef7fa1
Jorne <mr.jorne@gmail.com>
2021-01-03 11:12:46 +0100

    Projects.html

--- ImageFrame.js
+++ ImageFrame.js
@@ -1,33 +1,40 @@
-console.log("Image Frame loaded")
-
 const frame = Vue.createApp({
     //data, functions, template
-
-
     data() {
         return {
-            slide: 0,
-            //element array{title title 2 text image}
-            date: "00-0000",
-            title: "Something cool",
-            text: "Lorem ipsum bla bla bla bla Lorem ipsum bla bla bla bla Lorem ipsum bla bla bla bla Lorem ipsum bla bla bla bla Lorem ipsum bla bla bla bla",
-            image: '/Assets/Thinkpad.png'
+            slideId: 0,
+            title: "My",
+            title2: "Portofolio",
+            text: "On this page you can find projects i have worked on.",
+            image: '/Assets/Coding.gif',
+            slide: [
+                { title: "11-8888", title2: "TIITTTTLLEE", text: "this is a different text, this is a different text,  this is a different text,  this is a different text  this is a different text.   ", image: '/Assets/Amiga.gif' },
+                { title: "22-8888", title2: "TIITTTTLLEE", text: "this is a different text, this is a different text,  this is a different text,  this is a different text  this is a different text.   ", image: '/Assets/Amiga.gif' },
+                { title: "33-8888", title2: "TIITTTTLLEE", text: "this is a different text, this is a different text,  this is a different text,  this is a different text  this is a different text.   ", image: '/Assets/Amiga.gif' },
+
+            ]
         }
     },
     methods: {
         nextImage(next) {
             if (next) {
-                this.slide++
-                    console.log(this.slide)
+                this.slideId++
+                    if (this.slideId >= this.slide.length) {
+                        this.slideId = 0;
+                    }
             } else {
-                if (this.slide > 0) {
-                    this.slide--
-                        console.log(this.slide)
+                if (this.slideId > 0) {
+                    this.slideId--
+                } else {
+                    this.slideId = this.slide.length - 1;
                 }
             }
+            console.log(this.slideId)
+            this.title = this.slide[this.slideId].title;
+            this.title2 = this.slide[this.slideId].title2;
+            this.text = this.slide[this.slideId].text;
+            this.image = this.slide[this.slideId].image;
         }
     }
-
 })
-
 frame.mount('#frame')
